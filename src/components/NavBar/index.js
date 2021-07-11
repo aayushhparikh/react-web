561e882826656d2b8c17452dbe547678ca30375f
aayushhparikh <thedude5190@gmail.com>
2021-07-11 04:18:48 -0400

    added styled components and react router

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Nav, NavbarContainer, NavLogo } from './navBarElements'
+
+const Navbar = () => {
+    return (
+      <div>
+          <Nav>
+              <NavbarContainer>
+                  <NavLogo>Home</NavLogo>
+              </NavbarContainer>
+          </Nav>
+      </div>
+    )
+}
+
+export default Navbar
